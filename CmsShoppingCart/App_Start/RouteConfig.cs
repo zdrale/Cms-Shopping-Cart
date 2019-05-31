db3a475a3a899a7784bb675644ca700f5027e69f
nikolazd22 <nikolazd22@outlook.com>
2019-05-31 15:08:43 +0200

    Revert "front shop and cart done fn"
    This reverts commit fa8af06628375782676fbbf8dbb15f166b0fdcf9.

--- CmsShoppingCart/App_Start/RouteConfig.cs
+++ CmsShoppingCart/App_Start/RouteConfig.cs
@@ -13,23 +13,11 @@ namespace CmsShoppingCart
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "CmsShoppingCart.Controllers" });
-
-            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "CmsShoppingCart.Controllers" });
-
-            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "CmsShoppingCart.Controllers" });
-
-            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial",new { controller="Pages",action= "PagesMenuPartial" }, new[] {"CmsShoppingCart.Controllers" });
-            routes.MapRoute("Pages", "{pages}", new { controller = "Pages", action = "Index" }, new[] { "CmsShoppingCart.Controllers" });
-
-            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "CmsShoppingCart.Controllers" });
-
-
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
